70858da1d727239e78d89b2eb8c28eef192313a0
MichaelAug <michael.aug@protonmail.com>
2018-07-03 12:52:20 +0100

    Created the FileChangeChecker class

---
+++ YoCode/FileChangeChecker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace YoCode
+{
+    class FileChangeChecker
+    {
+        public static void CheckIfModified (FileStream originalFile, FileStream modifiedFile)
+        {
+
+        }
+    }
+}
