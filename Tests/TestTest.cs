a062fd3abc63cbb252b1c85eb4d84d94dd35edda
iLigonis <karmonasedvinas@gmail.com>
2018-07-03 10:31:56 +0100

    Added Xunit test with dummy test inside to test VSTS

---
+++ Tests/TestTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+
+namespace Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            true.Should().Be(false);
+        }
+    }
+}
