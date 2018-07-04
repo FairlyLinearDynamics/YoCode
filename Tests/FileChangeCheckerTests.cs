1f68f7b9413eec131e92edd60834d20b4f55867c
MichaelAug <michael.aug@protonmail.com>
2018-07-04 16:33:05 +0100

    amde chanegs

--- Tests/FileChangeCheckerTests.cs
+++ Tests/FileChangeCheckerTests.cs
@@ -23,4 +23,47 @@ namespace YoCode_XUnit
 
         }
     }
+
+    using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace YoCode
+    {
+
+
+        public class Directory : IDirectory
+        {
+            public List<String> OriginalPaths { get; }
+            public List<String> ModifiedPaths { get; }
+
+            public Directory(List<String> originalPaths, List<String> modifiedPaths)
+            {
+                OriginalPaths = originalPaths;
+                ModifiedPaths = modifiedPaths;
+            }
+
+            private List<Stream> ReturnPathFileStream(List<string> paths)
+            {
+                List<Stream> streamList = new List<Stream>();
+
+                for (int i = 0; i < paths.Count; i++)
+                {
+                    FileStream fs = File.Create(paths[i]);
+                    streamList.Add(fs);
+                }
+                return streamList;
+            }
+
+            public List<Stream> ReturnOriginalPathFileStream()
+            {
+                return ReturnPathFileStream(OriginalPaths);
+            }
+
+            public List<Stream> ReturnModifiedPathFileStream()
+            {
+                return ReturnPathFileStream(ModifiedPaths);
+            }
+        }
+    }
 }
---
+++ YoCode/IDirectory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace YoCode
+{
+    public interface IDirectory
+    {
+        List<string> ModifiedPaths { get; }
+        List<string> OriginalPaths { get; }
+
+        List<Stream> ReturnModifiedPathFileStream();
+        List<Stream> ReturnOriginalPathFileStream();
+    }
+}
