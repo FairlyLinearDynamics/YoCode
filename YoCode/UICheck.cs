014956123fa169e8306fd5c624a4caecd9e192d0
matas.zilaitis <matas.zilaitis@gmail.com>
2018-07-04 15:34:23 +0100

    Deleted a test file for FileImport class

---
+++ YoCode/UICheck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace YoCode
+{
+    public class UICheck
+    {
+        public static bool UIContainsFeature(String userFileURL, String[] keyWords)
+        {
+            var userFile = File.ReadAllText(userFileURL);
+            var comp = StringComparison.OrdinalIgnoreCase;
+
+            foreach(String key in keyWords)
+            {
+                if (userFile.Contains(key,comp))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
