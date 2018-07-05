e93d039d0637836229cdb97540f6fb9cce79c48e
matas.zilaitis <matas.zilaitis@gmail.com>
2018-07-05 12:38:16 +0100

    We can now check whether the user has checked git. next step refactoring

--- YoCode/GitCheck.cs
+++ YoCode/GitCheck.cs
@@ -31,7 +31,7 @@ namespace YoCode
             psi.WorkingDirectory = PATH;
             psi.FileName = GIT_INSTALL_DIRECTORY + @"\bin\git.exe";
             psi.Arguments = "log ";
-        
+
 
         }
 
@@ -41,10 +41,10 @@ namespace YoCode
         public void openProcess()
         {
             process = new Process();
-            process.StartInfo = psi;        
+            process.StartInfo = psi;
             process.Start();
         }
-        
+
 
         public string getOutput()
         {
@@ -56,11 +56,57 @@ namespace YoCode
             Console.Write(output);
         }
 
-    
-            
+        public void parseOutput(String output)
+        {
+            //getAuthorList(output);
+            Console.WriteLine(getLastAuthor(output));
+            Console.WriteLine(gitHasBeenUsed(getLastAuthor(output)));
+
 
         }
 
+        private List<String> getAuthorList(string output)
+        {
+            StringReader sr = new StringReader(output);
+            List<String> authors = new List<String>();
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (line.Contains("Author"))
+                {
+                    authors.Add(line);
+                }
+            }
+            return authors;
+        }
+
+        private string getLastAuthor(string output)
+        {
+            StringReader sr = new StringReader(output);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (line.Contains("Author"))
+                {
+                    return line;
+                }
+            }
+            return "No commits/authors found";
+        }
+
+        private bool gitHasBeenUsed(string lastAuthor)
+        {
+            if(lastAuthor.Contains("@nonlinear.com") || lastAuthor.Contains("@waters.com"))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+
+        }
 
 
 
