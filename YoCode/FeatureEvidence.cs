696002a98e006e4f1c5429d6423203fa668c8a92
iLigonis <karmonasedvinas@gmail.com>
2018-07-10 12:02:45 +0100

    Created feature evidence class

---
+++ YoCode/FeatureEvidence.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YoCode
+{
+    public class FeatureEvidence
+    {
+        public bool UIEvidencePresent { get; set; }
+        public string UIEvidence { get; set; }
+    }
+}
