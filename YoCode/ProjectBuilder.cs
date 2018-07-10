e009192e728cd9b50bb67529026c70ad83641a00
MichaelAug <michael.aug@protonmail.com>
2018-07-10 15:59:39 +0100

    Created new class for Building the applicant's code

---
+++ YoCode/ProjectBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YoCode
+{
+    class ProjectBuilder : ProcessRunner
+    {
+        public ProjectBuilder(string processName, string workingDir, string arguments) : base(processName, workingDir, arguments)
+        {
+        }
+    }
+}
