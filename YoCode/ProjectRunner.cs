671d4d47bf0e77fd117651c62b8c276e7b17f94a
MichaelAug <michael.aug@protonmail.com>
2018-07-13 12:28:32 +0100

    Added ProjectRunner class

---
+++ YoCode/ProjectRunner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace YoCode
+{
+    public class ProjectRunner
+    {
+        private string Process { get; } = "dotnet";
+        private string Argument { get; } = "run";
+        public string Output { get; }
+        public string ErrorOutput { get; set; }
+
+        public ProjectRunner(string workingDir)
+        {
+            ProcessRunner processRunner = new ProcessRunner(Process, workingDir, Argument);
+            processRunner.ExecuteTheCheck();
+            Output = processRunner.Output;
+            ErrorOutput = processRunner.ErrorOutput;
+
+            Console.WriteLine(Output);
+            Console.WriteLine("========================ERROR OUTPUT=================================");
+            Console.WriteLine(ErrorOutput);
+        }
+
+        //public static 
+    }
+}
