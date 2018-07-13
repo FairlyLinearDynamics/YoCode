275f114f56c4dee045d6c7cccfb165441211a931
iLigonis <karmonasedvinas@gmail.com>
2018-07-13 16:06:39 +0100

    Removed unnecessary comments

--- YoCode/CommandLineArguments/CommandExtractor.cs
+++ YoCode/CommandLineArguments/CommandExtractor.cs
@@ -12,24 +12,9 @@ namespace YoCode
             var argParts = arg.Split(new[] { CommandIdentifiers.commandIdentifier, CommandIdentifiers.dataIdentifier }, StringSplitOptions.RemoveEmptyEntries);
             return new SplitArg
             {
-                //command = (ExtractSuffix(arg,CommandIdentifiers.dataIdentifier) != null) 
-                //? ExtractPrefix(arg.Substring(0, arg.IndexOf(CommandIdentifiers.dataIdentifier)), CommandIdentifiers.commandIdentifier) 
-                //: ExtractPrefix(arg, CommandIdentifiers.commandIdentifier),
-
-                //data = ExtractSuffix(arg, CommandIdentifiers.dataIdentifier),
                 command =  argParts[0] ,
                 data = (argParts.Length>1) ? argParts[1] : null,
             };
-        }
-
-        private static string ExtractPrefix(string key, string commandIdentifier)
-        {
-            return (key.Contains(commandIdentifier) && key != null) ? key.Remove(0, commandIdentifier.Length) : null;
-        }
-
-        private static string ExtractSuffix(string key, string dataIdentifier)
-        {
-            return (key.Contains(dataIdentifier) && key != null) ? key.Remove(0, key.IndexOf(dataIdentifier) + 1) : null;
         }
     }
 }
