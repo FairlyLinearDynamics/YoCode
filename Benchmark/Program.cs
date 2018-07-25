18ee92463c3b086cdea6d17bf28cabaea490b094
iLigonis <karmonasedvinas@gmail.com>
2018-07-25 14:20:31 +0100

    Setting up benchmark testing

---
+++ Benchmark/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Benchmark
+{
+    public class DupfinderCheck
+    {
+        [Benchmark]
+        public void TestDupFinder()
+        {
+            new YoCode.DuplicationCheck();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
