7dda6d73ed1131abdb77a2f23cf96051eae5468b
matas.zilaitis <matas.zilaitis@gmail.com>
2018-08-17 10:47:21 +0100

    enum for testtype

---
+++ YoCode/ProcessDetailsStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YoCode
+{
+    class ProcessDetailsStorage
+    {
+        public ProcessDetailsStorage(string mode)
+        {
+
+        }
+
+
+
+    }
+}
