8435845f67df82ecb915b8e586129f352896867d
iLigonis <karmonasedvinas@gmail.com>
2018-08-21 16:47:23 +0100

    On browser close, browser object is set to null

--- YoCode/FrontEndCheck.cs
+++ YoCode/FrontEndCheck.cs
@@ -77,6 +77,7 @@ namespace YoCode
             {
                 browser.Dispose();
                 Running = false;
+                browser = null;
                 return true;
             }
 
