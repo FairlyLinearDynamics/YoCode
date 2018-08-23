00a6d2583502aab043699d669cf4ce1796e8de01
iLigonis <karmonasedvinas@gmail.com>
2018-08-23 15:02:25 +0100

    Improvements to the very important feature

--- YoCode/LoadingAnimation.cs
+++ YoCode/LoadingAnimation.cs
@@ -10,6 +10,8 @@ namespace YoCode
         static int cursorStartPos;
         static int cursorStopPos;
 
+        
+
         public static void RunLoading()
         {
             PrintIntro();
@@ -17,9 +19,11 @@ namespace YoCode
 
             var loadingBanner = new StringBuilder();
             var fireplaceFrames = new string[] { messages.ConsoleFireplaceBannerFrame1,
-                messages.ConsoleFireplaceBannerFrame2, messages.ConsoleFireplaceBannerFrame3};
+                messages.ConsoleFireplaceBannerFrame2, messages.ConsoleFireplaceBannerFrame3,
+                messages.ConsoleFireplaceBannerFrame4, messages.ConsoleFireplaceBannerFrame5};
 
             var fireplaceInd = 0;
+            var tick = 0;
 
             while (true)
             {
@@ -30,23 +34,10 @@ namespace YoCode
                 Console.WriteLine(messages.ParagraphDivider);
                 Console.WriteLine();
 
-                //Console.Write(loadingBanner);
-
                 Console.Write(String.Format("Loading{0,-3}", dots));
                 cursorStopPos = Console.CursorTop;
-                dots += ".";
-                if (dots.Equals("...."))
-                {
-                    dots = "";
-                }
-                if (LoadingFinished)
-                {
-                    ClearLine();
-                    Console.CursorTop = cursorStartPos;
-                    break;
-                }
 
-                if (fireplaceInd > 1)
+                if (fireplaceInd > 3)
                 {
                     fireplaceInd = 0;
                 }
@@ -54,7 +45,33 @@ namespace YoCode
                 {
                     fireplaceInd++;
                 }
-                Thread.Sleep(1000);
+
+                if (tick > 8)
+                {
+                    LoadingDotsFire();
+                    tick = 0;
+                }
+                else
+                {
+                    tick++;
+                }
+
+                if (LoadingFinished)
+                {
+                    ClearLine();
+                    Console.CursorTop = cursorStartPos;
+                    break;
+                }
+                Thread.Sleep(125);
+            }
+        }
+
+        private static void LoadingDotsFire()
+        {
+            dots += ".";
+            if (dots.Equals("...."))
+            {
+                dots = "";
             }
         }
 
