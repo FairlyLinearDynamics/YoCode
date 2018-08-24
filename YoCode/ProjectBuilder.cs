a389d2605947826871f8044189e0b08d45c59f9c
iLigonis <karmonasedvinas@gmail.com>
2018-08-24 16:45:07 +0100

    Changed error message

--- YoCode/ProjectBuilder.cs
+++ YoCode/ProjectBuilder.cs
@@ -70,7 +70,7 @@ namespace YoCode
         {
             if(Output.Contains("is being used by another process"))
             {
-                ProjectBuilderEvidence.SetInconclusive(messages.BadPort);
+                ProjectBuilderEvidence.SetInconclusive("Could not project's build status. It is being used by another process");
                 return;
             }
             buildSuccessful = Output.Contains("Build succeeded");
