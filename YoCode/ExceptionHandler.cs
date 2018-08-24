63a8f1de38e71e7c10fb050c1e241944e5594a3c
MichaelAug <michael.aug@protonmail.com>
2018-08-24 11:41:32 +0100

    Added an unhandled exception handler which tries to close selenium and unitconverterwebapp if an unhandled exception occurs

---
+++ YoCode/ExceptionHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace YoCode
+{
+    internal static class ExceptionHandler
+    {
+        public static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            ConsoleCloseHandler.CloseProcesses();
+
+            Debug.WriteLine((e.ExceptionObject as Exception)?.Message);
+        }
+    }
+}
