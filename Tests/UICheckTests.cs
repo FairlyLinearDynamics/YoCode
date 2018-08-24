47d275cf57717792517e50c656034139205a7742
iLigonis <karmonasedvinas@gmail.com>
2018-08-24 16:57:19 +0100

    Readded UICheck and test

---
+++ Tests/UICheckTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+using FluentAssertions;
+using YoCode;
+
+namespace YoCode_XUnit
+{
+    public class UICheckTests
+    {
+        private readonly string fakeFilePath = @"..\..\..\TestData\MockHTML.cshtml";
+        private readonly string[] keyWords = { "miles", "kilometer" };
+
+        // Write better testing mehod
+        [Fact]
+        public void UICheck_FeatureImplementedBoolCheck()
+        {
+            var uiCheck = new UICheck(fakeFilePath, keyWords);
+
+            var evidence = uiCheck.UIEvidence;
+
+            evidence.FeatureImplemented.Should().Be(true);
+        }
+
+        [Fact]
+        public void UICheck_FeatureEvidencePresent()
+        {
+            var uiCheck = new UICheck(fakeFilePath, keyWords);
+
+            var evidence = uiCheck.UIEvidence;
+
+            evidence.EvidencePresent.Should().Be(true);
+        }
+
+        [Fact]
+        public void UICheck_FeatureTitleSet()
+        {
+            var uiCheck = new UICheck(fakeFilePath, keyWords);
+
+            var evidence = uiCheck.UIEvidence;
+
+            evidence.FeatureTitle.Should().NotBeEmpty();
+        }
+    }
+}
---
+++ YoCode/UICheck.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace YoCode
+{
+    internal class UICheck
+    {
+        // -------------------------------------------------------------------------------------------- Constructors
+        public UICheck(IEnumerable<string> userFilePaths, string[] keyWords)
+        {
+            UIContainsFeature(userFilePaths, keyWords);
+            UIEvidence.FeatureTitle = "Evidence present in UI";
+            UIEvidence.Feature = Feature.UICheck;
+        }
+
+        public UICheck(string userFilePath, string[] keyWords) : this(new List<string> { userFilePath }, keyWords)
+        {
+            UIContainsFeature(userFilePath, keyWords);
+        }
+
+        // -------------------------------------------------------------------------------------------- Single UI check
+        private void UIContainsFeature(string userFilePath, string[] keyWords)
+        {
+            var userFile = File.ReadAllLines(userFilePath);
+
+            //ListOfMatches.add
+
+            for (var i = 0; i < userFile.Length; i++)
+            {
+                if (ContainsKeyWord(userFile[i], keyWords))
+                {
+                    UIEvidence.FeatureImplemented = true;
+                    UIEvidence.FeatureRating = 1;
+
+                    UIEvidence.GiveEvidence($"Found  on line {i + 1} in file \\{new DirectoryInfo(userFilePath).Parent.Name}\\{Path.GetFileName(userFilePath)}");
+                }
+            }
+        }
+
+        // -------------------------------------------------------------------------------------------- List of UI to check
+        private void UIContainsFeature(IEnumerable<string> userFilePaths, string[] keyWords)
+        {
+            foreach (var path in userFilePaths)
+            {
+                UIContainsFeature(path, keyWords);
+            }
+        }
+
+        // -------------------------------------------------------------------------------------------- Check logic
+        private static bool ContainsKeyWord(string line, IEnumerable<string> keyWords)
+        {
+            //Console.WriteLine(keyWords.Select(key => line.ToLower().Contains(key)));
+            var words = Regex.Split(line, "[^A-Za-z0-9]").ToList();
+
+            return words.Any(word => keyWords.ToList().Any(keyword => word.Equals(keyword, StringComparison.OrdinalIgnoreCase)));
+        }
+
+        // -------------------------------------------------------------------------------------------- Return methods
+        public FeatureEvidence UIEvidence { get; } = new FeatureEvidence();
+    }
+}
